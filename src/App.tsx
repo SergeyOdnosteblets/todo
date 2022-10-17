45ec2f3d198b3933a69c1b4763066a7e8fe62ad7
SergeyOdnosteblets <59088833+SergeyOdnosteblets@users.noreply.github.com>
2022-10-17 10:11:42 +0300

    update design

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import React, { useState } from 'react';
 import { Todo } from './Components/Todo/Todo';
 import './App.scss';
 
-import { Checkbox } from '@nextui-org/react';
-
 export const App = () => {
   const [backGround, setBackGround] = useState(false);
 
   return (
     <div className={backGround ? 'App light' : 'App dark'}>
       <Todo setBackGround={setBackGround} backGround={backGround} />
-      <div className='test'>
-        <Checkbox isRounded={true} color="secondary">
-          Rounded option
-        </Checkbox>
-      </div>
     </div>
   );
 };
